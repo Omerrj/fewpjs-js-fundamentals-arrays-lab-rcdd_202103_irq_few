8d776a9c63d8a4593051d36af5de779419d7190a
Omer Sabah <omerrj99@gmail.com>
2021-04-10 11:01:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const append=["Milo", "Otis", "Garfield"];
 const prepend=["Milo", "Otis", "Garfield"];
 const removeLast=["Milo", "Otis", "Garfield"];
 const removeFirst=["Milo", "Otis", "Garfield"];
+
+append.push('Odie');
+append.unshift('Odie');
+append.pop('Odie');
+append.push('Odie');
